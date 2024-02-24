1cb8657928e592c1af9c9b94e92685ed05985626
mahimaro2212 <mahimaro2212@gmail.com>
2024-02-24 18:30:53 +0900

    [3の倍数]を学習しました。

--- 01_テキスト/01_JavaScriptのオブジェクト/main.js
+++ 01_テキスト/01_JavaScriptのオブジェクト/main.js
@@ -15,12 +15,24 @@ const hikakin = {
 // TRY: 練習問題１
 // hikakin オブジェクト を真似して自分のプロフィールをオブジェクトで作ってください。
 // オブジェクトにはたくさんのプロパティを追加してください。
-
-// const me = { name: 'あなたのなまえ' }
+const me = {
+  name: "まひろ",
+  age: 21,
+  hobbies: ["バスケットボール", "駅伝観戦"],
+  isHappy: true,
+  address: {
+    country: "日本",
+    prefecture: "埼玉",
+    city: "川越",
+  },
+  sayHello: function () {
+    console.log("おはようございます")
+  },
+}
 
 // ブラウザのコンソールを開いてオブジェクトが正しく作れているか確認してみましょう👀
-
-// console.log(me)
+console.log(me)
 
 // TRY: 練習問題２
 // hikakin オブジェクトに入っている sayHello メソッドを実行してください。
+me.sayHello()
---
+++ 02_レシピ/01_3の倍数/main.js
@@ -0,0 +1,18 @@
+// const number = 24
+// for (let n = 1; n <= number; n++) {
+//   if (n % 3 == 0) {
+//     console.log(n + "!!!!!!!")
+//   } else {
+//     console.log(n)
+//   }
+// }
+number = 30
+const genkiFunction = function (number) {
+  for (let n = 1; n <= number; n++) {
+    if (n % 3 == 0) {
+      console.log(n + "!!!!!!!")
+    } else {
+      console.log(n)
+    }
+  }
+}
