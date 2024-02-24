7793e091c4ce6073718bb2ab24e8a29ab0deb81e
mahimaro2212 <mahimaro2212@gmail.com>
2024-02-24 20:47:08 +0900

    [3の倍数]を学習しました。

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -1,3 +1,4 @@
+//24までの値
 // const number = 24
 // for (let n = 1; n <= number; n++) {
 //   if (n % 3 == 0) {
@@ -6,13 +7,70 @@
 //     console.log(n)
 //   }
 // }
-number = 30
-const genkiFunction = function (number) {
-  for (let n = 1; n <= number; n++) {
-    if (n % 3 == 0) {
-      console.log(n + "!!!!!!!")
-    } else {
-      console.log(n)
-    }
+
+//関数での定義
+// const genkiFunction = function (number) {
+//   for (let n = 1; n <= number; n++) {
+//     if (n % 3 == 0) {
+//       console.log(n + "!!!!!!!")
+//     } else {
+//       console.log(n)
+//     }
+//   }
+// }
+
+//3,5,15の倍数判断：FizzBuzz
+// const FizzBuzz = function (number) {
+//   for (let i = 1; i <= number; i++) {
+//     if (i % 15 == 0) {
+//       console.log("FizzBuzz")
+//     } else if (i % 5 == 0) {
+//       console.log("Buzz")
+//     } else if (i % 3 == 0) {
+//       console.log("Fizz")
+//     } else {
+//       console.log(i)
+//     }
+//   }
+// }
+
+//3が含まれるものも元気に(未完成)
+// const genkiFunction = function (number) {
+//   for (let n = 1; n <= number; n++) {
+//     if (n % 3 == 0) {
+//       console.log(n + "!!!!!!!")
+//     } else {
+//       console.log(n)
+//     }
+//   }
+// }
+
+//元気な秒針（setInterval）
+const display = document.getElementById("display")
+const button = document.getElementById("button")
+
+let count = 0
+
+const countUp = function () {
+  count += 1
+  if (count % 3 == 0) {
+    display.textContent = count + "!!!!!!!"
+  } else {
+    display.textContent = count
+  }
+}
+
+let id = null
+
+button.onclick = function () {
+  if (id === null) {
+    // start
+    id = setInterval(countUp, 1000)
+    button.textContent = "stop"
+  } else {
+    // stop
+    clearInterval(id)
+    id = null
+    button.textContent = "start"
   }
 }
